cb604b4ae6f931c5a5b18428219315fcb9fd30e3
sooyeoniya <tndus1306403@naver.com>
2023-11-21 11:50:24 +0900

    Refactoring root folder

---
+++ src/component/footer.php
@@ -0,0 +1,12 @@
+        <div id="footer_content">
+            <p id="footer_logo">웹프로그래밍 Term Project <span>최수연</span></p>
+            <ul id="download">
+                <li>최수연</li>
+                <li>- 컴퓨터공학부</li>
+                <li>- 2020136129</li>
+            </ul>
+            <ul id="author">
+                <li>저자 문의 메일</li>
+                <li>- 메일 주소 : tndus502@koreatech.ac.kr</li>
+            </ul>
+        </div>
---
+++ src/index/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8">
+<title>🌸</title>
+<link rel="stylesheet" type="text/css" href="./css/common.css?after">
+<link rel="stylesheet" type="text/css" href="./css/main.css?after">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
+<script type="text/javascript" src="./js/date.js" defer></script>
+</head>
+<body> 
+	<header>
+    	<?php include "header.php";?>
+    </header>
+	<section>
+	    <?php include "main.php";?>
+	</section> 
+	<footer>
+    	<?php include "footer.php";?>
+    </footer>
+</body>
+</html>
